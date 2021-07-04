590b1f75093dc66ccc366b9402be08b6d3aadd9e
ryfazrin <riansera515@gmail.com>
2021-07-04 14:46:53 +0800

    first commit

---
+++ app/src/main/java/com/example/cruddengansqlite/UpdateActivity.java
@@ -0,0 +1,52 @@
+package com.example.cruddengansqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class UpdateActivity extends AppCompatActivity {
+    protected Cursor cursor;
+    Database database;
+    Button btn_simpan;
+    EditText nama, kampus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+        database = new Database(this);
+        nama = findViewById(R.id.nama);
+        kampus = findViewById(R.id.kampus);
+        btn_simpan = findViewById(R.id.btn_simpan);
+
+        SQLiteDatabase db = database.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM mahasiswa WHERE nama = '" +
+                getIntent().getStringExtra("nama") + "'", null);
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            cursor.moveToPosition(0);
+            nama.setText(cursor.getString(0).toString());
+            kampus.setText(cursor.getString(1).toString());
+        }
+
+        btn_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = database.getWritableDatabase();
+                db.execSQL("UPDATE mahasiswa set nama = '" +
+                        nama.getText().toString() + "', kampus = '" +
+                        kampus.getText().toString() + "' WHERE nama = '" +
+                        getIntent().getStringExtra("nama")+ "'");
+                Toast.makeText(UpdateActivity.this, "Data berhasil diupdate", Toast.LENGTH_SHORT).show();
+                MainActivity.ma.RefreshList();
+                finish();
+            }
+        });
+    }
+}
